a29e0d859fb7aadd6628a1a324a7c6dcf6c78136
Samuel Martinez <123973975+SamuelM45@users.noreply.github.com>
2024-11-15 18:35:35 +0000

    Part 4 and 5

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('post/<int:id>/', views.post_detail, name='post_detail'), 
     path('post/new/', views.post_create, name='post_create'),  
     path('post/<int:id>/edit/', views.post_edit, name='post_edit'), 
+    path('post/<int:id>/delete/', views.post_delete, name='post_delete'),
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,39 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import BlogPost
-
+from .forms import BlogPostForm
 
 def post_list(request):
     posts = BlogPost.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
 def post_detail(request, id):
     post = get_object_or_404(BlogPost, id=id)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-
 def post_create(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        BlogPost.objects.create(title=title, content=content)
-        return redirect('post_list')
-    return render(request, 'blog/post_form.html')
-
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = BlogPostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
 
 def post_edit(request, id):
     post = get_object_or_404(BlogPost, id=id)
     if request.method == 'POST':
-        post.title = request.POST.get('title')
-        post.content = request.POST.get('content')
-        post.save()
-        return redirect('post_detail', id=post.id)
-    return render(request, 'blog/post_form.html', {'post': post})
+        form = BlogPostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form, 'post': post})
+
+def post_delete(request, id):
+    post = get_object_or_404(BlogPost, id=id)
+    if request.method == 'POST':  # Ensure the delete action is POSTed
+        post.delete()
+        return redirect('post_list')
+    return redirect('post_list')  # Redirect to post list if method is not POST
